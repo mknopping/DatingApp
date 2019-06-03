bb256daac9a4fd3a86c6e6ced68bdd541342d4e9
Max Knopping <mknopping@classlink.com>
2019-06-03 11:05:37 -0400

    end section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DatingApp.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
